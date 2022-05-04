d518cead4b03065f1aeb965e84545a2bf74fd4a4
jpickard393 <jpickard@and.digital>
2022-05-04 20:19:55 +0100

    ch5-p-106 added test to remove subscriber

--- src/main/java/com/practicalUnitTesting/RaceResultService.java
+++ src/main/java/com/practicalUnitTesting/RaceResultService.java
@@ -1,14 +1,18 @@
 package com.practicalUnitTesting;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 
 public class RaceResultService {
     // private Client client;  this failed as there were more than one client
-    private Collection<Client> clients = new ArrayList<Client>();
+    private Collection<Client> clients = new HashSet<Client>();
     public void addSubscriber(Client client) {
         clients.add(client);
         // this.client = client;
+    }
+
+    public void removeSubscriber(Client client){
+        clients .remove(client);
     }
 
     public void send(Message message) {
--- src/test/java/com/practicalUnitTesting/ResultsServiceFourthandFifthTest.java
+++ src/test/java/com/practicalUnitTesting/ResultsServiceFourthandFifthTest.java
@@ -1,11 +1,10 @@
 package com.practicalUnitTesting;
 
 import org.junit.jupiter.api.Test;
-
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.mock;
 
-public class RaceResultsSendMessagesToSubscribersOnly {
+public class ResultsServiceFourthandFifthTest {
     RaceResultService raceResults = new RaceResultService();
     Message message = mock(Message.class);
     Client clientA = mock(Client.class, "clientA");
@@ -16,5 +15,21 @@ public class RaceResultsSendMessagesToSubscribersOnly {
         raceResults.send(message);
         verify(clientA, never()).receive(message);
         verify(clientB, never()).receive(message);
+    }
+
+    @Test
+    void shouldSendOnlyOneMessageToMultiSubscriber() {
+        raceResults.addSubscriber((clientA));
+        raceResults.addSubscriber((clientA));
+        raceResults.send(message);
+        verify(clientA).receive(message);
+    }
+
+    @Test
+    void unSubscribedClientStopsReceivingMessaged() {
+        raceResults.addSubscriber(clientA);
+        raceResults.removeSubscriber(clientA);
+        raceResults.send(message);
+        verify(clientA, never()).receive(message);
     }
 }
